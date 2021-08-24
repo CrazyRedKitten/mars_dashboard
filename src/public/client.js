9730d06d20c28d0c79c3d2dea45848ec14b7df8e
Aleksandr Shchilkin <crazyredkitten@icloud.com>
2021-08-25 02:27:08 +0300

    frontend setup

--- src/public/client.js
+++ src/public/client.js
@@ -1,141 +1,167 @@
-let store = {
-    user: { name: "Student" },
-    apod: '',
-    rovers: ['Curiosity', 'Opportunity', 'Spirit'],
-    roverData: {},
-    activeRover: "",
-}
+/* eslint-disable camelcase */
+/* eslint-disable new-cap */
+/* eslint-disable valid-jsdoc */
+
+// TODO: Add Immutable.js
+const store = {
+  app: {name: 'Mars Dashboard'},
+  user: {name: 'Student'},
+  apod: '',
+  rovers: ['Curiosity', 'Opportunity', 'Spirit'],
+  roverData: {},
+  activeRover: undefined,
+};
 
 // add our markup to the page
-const root = document.getElementById('root')
+const root = document.getElementById('root');
 
 const updateStore = (store, newState) => {
-    store = Object.assign(store, newState)
-    render(root, store)
-}
+  store = Object.assign(store, newState);
+  render(root, store);
+};
 
 const render = async (root, state) => {
-    root.innerHTML = App(state)
-}
+  root.innerHTML = App(state);
+};
 
 
 // create content
 const App = (state) => {
-    let { rovers, apod, roverData } = state
+  const {rovers, roverData, activeRover} = state;
 
-    return `
-        <nav class="navbar navbar-light bg-light">
-            <div class="container-fluid">
-                <span class="navbar-brand mb-0 h1">Mars Dashboard</span>
-            </div>
-        </nav>
-        <header></header>
+  return `
+        ${renderNavbar(state)}
+        ${renderRoverInfo(state)}
         <main>
             <div class="d-flex justify-content-center">
-                <div id="rover-group" class="btn-group" role="group" aria-label="Basic example">
+                <div id="rover-group" class="btn-group" role="group" 
+                aria-label="Basic example">
                     ${ rovers && renderRovers(rovers)}
                 </div>
             </div>
             <section>
-                ${ImageOfTheDay(apod)}
-            </section>
-            <section>
-                ${RoverInfo(roverData)}
+                ${RoverCard(activeRover)}
+                ${RoverAPIData(roverData)}
+                ${getRoverInformation(roverData)}
             </section>
         </main>
-        <footer></footer>
-    `
-}
+    `;
+};
+
+/**
+ * Renders navbar
+ * @param {object} state Application state
+ * @return {string} Navbar HTML Markup
+ */
+const renderNavbar = (state) => {
+  const {app} = state;
+
+  return (`
+        <nav class="navbar navbar-light bg-light">
+            <div class="container-fluid">
+                <span class="navbar-brand mb-0 h1">${app.name}</span>
+            </div>
+        </nav>
+    `);
+};
+
+const renderRoverInfo = (state) => {
+  const {activeRover} = state;
+
+  if (typeof activeRover !== undefined) {
+    // await getRoverData()
+    return '';
+  }
+
+  return 0;
+};
 
 // listening for load event because page should load before any JS is called
 window.addEventListener('load', () => {
-    render(root, store)
-})
+  render(root, store);
+});
 
 // ------------------------------------------------------  COMPONENTS
 
-// Pure function that renders conditional information -- THIS IS JUST AN EXAMPLE, you can delete it.
-const Greeting = (name) => {
-    if (name) {
-        return `
-            <h1>Welcome, ${name}!</h1>
-        `
-    }
+const RoverCard = (roverIndex) => {
+  const {rovers} = store;
+  return `<h1>Rover Information: ${rovers[roverIndex]}</h1>`;
+};
 
-    return `
-        <h1>Hello!</h1>
-    `
-}
+const setRover = (roverName) => {
+  const roverIndexes = {
+    Curiosity: 0,
+    Opportunity: 1,
+    Spirit: 2,
+  };
+
+  switch (roverName) {
+    case 'Curiosity':
+      updateStore(store, {activeRover: roverIndexes.Curiosity});
+      break;
+    case 'Opportunity':
+      updateStore(store, {activeRover: roverIndexes.Opportunity});
+      break;
+    case 'Spirit':
+      updateStore(store, {activeRover: roverIndexes.Spirit});
+      break;
+    default:
+      return null;
+  }
+};
 
 /**
- * 
+ *
  * @param {array} rovers - an array of available rovers
- * @returns rovers
+ * @return rovers
  */
 const renderRovers = (rovers) => {
-    let htmlContent = '';
-    rovers.forEach(rover => {htmlContent += `<button type="button" class="btn btn-primary">${rover}</button>`})
-    return htmlContent;
-}
+  let htmlContent = '';
+  rovers.forEach((rover) => {
+    htmlContent += `<button type="button"
+    onclick="setRover('${rover}')" 
+    class="btn btn-primary">${rover}</button>`;
+  });
+  return htmlContent;
+};
 
+/**
+ *
+ * @param {object} roverData data in application store
+ * @return {object} data from NASA's API
+ */
+const RoverAPIData = (roverData) => {
+  const isEmptyRoverData = Object.keys(roverData).length === 0;
 
-// TODO: Remove example of pure funxtion
-// Example of a pure function that renders infomation requested from the backend
-const ImageOfTheDay = (apod) => {
+  // Handle empty object
+  if (isEmptyRoverData) {
+    // TODO: Get data about all rovers
+    getRoverData('curiosity');
+  } else {
+    console.log(roverData['latest_photos'][0].rover);
+    return ('ROVER DATA', roverData);
+  }
+};
 
-    // If image does not already exist, or it is not from today -- request it again
-    const today = new Date()
-    const photodate = new Date(apod.date)
-    console.log(photodate.getDate(), today.getDate());
+/**
+ * Returns rover data object
+ * @param {object} data - latest_photos data from NASA API
+ * @return {object} rover - information about rover
+ */
+const getRoverInformation = (data) => {
+  const isRoverDataEmpty = Object.keys(data).length === 0;
+  // Handle empty object
+  if (!isRoverDataEmpty) {
+    return data['latest_photos'][0].rover;
+  };
+};
 
-    console.log(photodate.getDate() === today.getDate());
-    if (!apod || apod.date === today.getDate() ) {
-        getImageOfTheDay(store)
-    }
-
-    // check if the photo of the day is actually type video!
-    if (apod) {
-        if (apod.media_type === "video") {
-            return (`
-                <p>See today's featured video <a href="${apod.url}">here</a></p>
-                <p>${apod.title}</p>
-                <p>${apod.explanation}</p>
-            `)
-        } else {
-            return (`
-                <img src="${apod.image.url}" height="350px" width="100%" />
-                <p>${apod.image.explanation}</p>
-            `)
-        }
-    }
-}
-
-const RoverInfo = (roverData) => {
-    const isEmptyRoverData = Object.keys(roverData).length === 0;
-
-    // Handle empty object
-    if (isEmptyRoverData) {
-        getRoverData('curiosity');
-    } else {
-        console.log(roverData.latest_photos);
-        return(`<code>${roverData.latest_photos}</code>`);
-    }
-}
 // ------------------------------------------------------  API CALLS
 
-// Example API call
-// TODO: Remove example
-const getImageOfTheDay = () => {
-    fetch(`/apod`)
-        .then(res => res.json())
-        .then(apod => updateStore(store, { apod }))
-}
-
 const getRoverData = (roverName) => {
-    fetch(`/rover/${roverName}`)
-        .then(res => res.json())
-        .then(roverData => {
-            console.log(roverData);
-            updateStore(store, {roverData});
-        });
-}
+  fetch(`/rover/${roverName}`)
+      .then((res) => res.json())
+      .then((roverData) => {
+        updateStore(store, {roverData});
+      });
+};
--- src/server/index.js
+++ src/server/index.js
@@ -1,42 +1,42 @@
-require('dotenv').config()
-const express = require('express')
+require('dotenv').config();
+const express = require('express');
 // removed bodyParser hence it is deprecated sinse express v. 4.16.0
 // const bodyParser = require('body-parser')
-const fetch = require('node-fetch')
-const path = require('path')
+const fetch = require('node-fetch');
+const path = require('path');
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
-app.use('/', express.static(path.join(__dirname, '../public')))
+app.use('/', express.static(path.join(__dirname, '../public')));
 
 // your API calls
 
 // example API call
 app.get('/apod', async (req, res) => {
-    try {
-        let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
-            .then(res => res.json())
-        res.send({ image })
-    } catch (err) {
-        console.log('error:', err);
-    }
+  try {
+    const image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
+        .then((res) => res.json());
+    res.send({image});
+  } catch (err) {
+    console.log('error:', err);
+  }
 });
 
 app.get('/rover/:name', async (req, res) => {
-    let { name } = req.params;
-    try {
-        const data = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${name}/latest_photos?api_key=${process.env.API_KEY}`).then(res => res.json());
-        
-        console.log(data)
+  const {name} = req.params;
+  try {
+    const data = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${name}/latest_photos?api_key=${process.env.API_KEY}`).then((res) => res.json());
 
-        res.send(data);
-    } catch (err) {
-        console.log('error:', err)
-    }
+    console.log(data);
+
+    res.send(data);
+  } catch (err) {
+    console.log('error:', err);
+  }
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
